ba90462faaf25dd20b3d7c41a891e9eb781ebaf8
carinadidomenico <carinadidomenico@gmail.com>
2020-06-24 22:44:38 -0300

    App java

---
+++ Aplicação Java/src/main/java/SlackAPI/SlackMessage.java
@@ -0,0 +1,33 @@
+
+package SlackAPI;
+
+import lombok.*;
+import java.io.Serializable;
+  
+@AllArgsConstructor
+@Builder(builderClassName = "Builder")
+@Getter
+@Setter
+
+public class SlackMessage implements Serializable {
+        
+    private String username;
+    private String text;
+    private String channel;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getText() {
+        return text;
+    }
+    
+    public String getChannel() {
+        return channel;
+    }
+          
+    
+    
+}
